af4466c2321c504b2c4ab123fa1a627cd8572c78
rahulkgupta1 <rahulkgupta477@gmail.com>
2024-12-19 08:17:57 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,39 @@
+const name = "rahul"
+const repoCount = 50
+
+//console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);//`` backtics
+
+const gameName = new String('rajal-hc-com')
+
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('a'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-5,7);
+console.log(anotherString);
+
+const newStringOne = "   hitesh  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary" // space  ki jagah %20 aa jata hei
+
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('sundar'));
+
+console.log(gameName.split('-'))
+
+console.log(gameName.bold())
+
+
