1aeba65a85d0f366cd4b0b236e559265eafe7ba4
rahulkgupta1 <rahulkgupta477@gmail.com>
2024-12-22 08:43:44 +0000

    Scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,60 @@
+// var c = 300
+let a = 300 
+if(true) {
+    let a = 10
+    const b = 20
+    //console.log("Inne:r ", a);
+    
+}
+
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+
+
+function one(){
+    const userName = "Rahul"
+
+    function two(){
+        const website = "outube"
+       // console.log(userName); 
+    }
+    //console.log(website);
+
+    two()
+}
+
+//one()
+
+if (true) {
+    const username = "Rahul"
+    if (username === "Rahul") {
+        const website = "youtube"
+        //console.log(username + website);
+        
+    }    
+    //console.log(website);
+    
+}
+
+//console.log(username);
+
+//***********inteeresting ++++++++++= */
+
+console.log(addone(5));
+
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+//console.log(addTwo(5))
+
