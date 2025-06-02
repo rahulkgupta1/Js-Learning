a504bd0abcce45e9a70307f57897cb924ead3872
Rahul Kumar Gupta <rahulkgupta477@gmail.com>
2025-06-02 01:30:57 +0000

    final updates

---
+++ 10_classes_and_oop/inheritence.js
@@ -0,0 +1,11 @@
+class User {
+    constructor(Usernamee){
+        this.Username = Username
+    }
+
+    logMe(){
+        console.log(`USERNAME is ${this.Username}`);
+    }
+
+class Taecher extens User
+}
